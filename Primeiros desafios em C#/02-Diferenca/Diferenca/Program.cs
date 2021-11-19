cebc3e1933986b80f28bff2ed7542137f57de5dc
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2021-11-19 15:16:45 -0300

    Commit dos Primeiros desafios em C#

---
+++ Primeiros desafios em C#/02-Diferenca/Diferenca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Diferenca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;   //declare suas variaveis              ;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+ 
+            //continue a solução
+            Console.WriteLine("DIFERENCA = " + ( a * b - c * d));
+            Console.ReadKey();
+        }
+    }
+}
