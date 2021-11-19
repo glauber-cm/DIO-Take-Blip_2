483e8e4e523526cd9c49655b19531d7e4b55e3c2
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2021-11-19 15:23:35 -0300

    Commit Desafios Intermediários em C#

---
+++ Desafios intermediarios C#/02-PreenchimentoDeVetorUm/PreenchimentoDeVetorUm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PreenchimentoDeVetorUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorLido = int.Parse(Console.ReadLine());
+            int[] n = new int[10];
+            for (int i = 0; i < n.Length; i++)
+            {
+                n[i] = valorLido;
+                Console.WriteLine("N[" + i +  "] = " + n[i]);
+                valorLido = valorLido * 2;
+
+            }
+        }
+    }
+}
---
+++ Desafios intermediarios C#/03-TrigoNoTabuleiro/TrigoNoTabuleiro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace TrigoNoTabuleiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong result_final = 0;
+            BigInteger result, quantidade;
+            
+            int cont;
+            int qtdTeste = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qtdTeste; i++)
+            {
+                result = 0;
+                quantidade = 12;
+                cont = 0;
+                int casas = int.Parse(Console.ReadLine()) - 1;
+                do
+                {
+                    quantidade = quantidade * 2;
+                    result = result + quantidade;
+                    cont++;
+
+                } while ( cont < casas);
+                result = result / 12000;
+                result = result / 12;
+                result_final = (ulong)result;
+                Console.WriteLine($"{result_final} kg");
+            }
+        }
+    }
+}
